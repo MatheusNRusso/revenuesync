54dc10249e7819cb4ac1fb8cb20593f0e0c97ec5
MatheusNRusso <mtnrs@hotmail.com>
2026-03-09 23:21:20 -0300

    feat: implement Conversion read API for Angular dashboard

--- src/main/java/com/mtnrs/revenuesync/repository/PaymentRepository.java
+++ src/main/java/com/mtnrs/revenuesync/repository/PaymentRepository.java
@@ -1,7 +1,6 @@
 package com.mtnrs.revenuesync.repository;
 
 import com.mtnrs.revenuesync.domain.Payment;
-import io.micrometer.observation.ObservationFilter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
